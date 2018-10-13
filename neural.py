8013b81f2efa311bef6b50edbf17f90acd1a6815
jhe911 <42699709+jhe911@users.noreply.github.com>
2018-10-12 22:56:52 -0400

    Add files via upload

---
+++ neural.py
@@ -0,0 +1,21 @@
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+# fill example_sent
+def filter(audio_string)
+    example_sent = audio_string
+   
+
+    stop_words = set(stopwords.words('english'))
+
+    word_tokens = word_tokenize(example_sent)
+
+    filtered_sentence = [w for w in word_tokens if not w in stop_words]
+
+    filtered_sentence = []
+
+    for w in word_tokens:
+        if w not in stop_words:
+            filtered_sentence.append(w)
+    print (example_sent)
+    print(word_tokens)
+    print(filtered_sentence)
